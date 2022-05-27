212e3d0a4dace8c8ef5794d67f630a6422acd14d
mobelman <34746491+mobelman@users.noreply.github.com>
2022-05-27 18:13:41 +0200

    Fixed issues.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ChurchComponent} from './churches/church/church.component';
+import {ProductionComponent} from './production/production.component';
+import {ContactComponent} from './contact/contact.component';
+import {PublicProductsComponent} from './public-products/public-products.component';
+import {PublicProductComponent} from './public-products/public-product/public-product.component';
+import {PublicChurchProductsComponent} from './public-church-products/public-church-products.component';
+import {PublicChurchProductComponent} from './public-church-products/public-church-product/public-church-product.component';
+import {PrivateProductsComponent} from './private-products/private-products.component';
+import {PrivateProductComponent} from './private-products/private-product/private-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'churches/:location/:name', component: ChurchComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'private/products', component: PrivateProductsComponent},
+  {path: 'private/products/:id', component: PrivateProductComponent},
+  {path: 'production', component: ProductionComponent},
+  {path: 'public/church/products', component: PublicChurchProductsComponent},
+  {path: 'public/church/products/:id', component: PublicChurchProductComponent},
+  {path: 'public/products', component: PublicProductsComponent},
+  {path: 'public/products/:id', component: PublicProductComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '**', redirectTo: '/dashboard'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ChurchesComponent} from './churches/churches.component';
+import {AppRoutingModule} from './/app-routing.module';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ProductionComponent} from './production/production.component';
+import {ChurchComponent} from './churches/church/church.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {PublicProductsComponent} from './public-products/public-products.component';
+import {PublicChurchProductsComponent} from './public-church-products/public-church-products.component';
+import {PublicProductComponent} from './public-products/public-product/public-product.component';
+import {PublicChurchProductComponent} from './public-church-products/public-church-product/public-church-product.component';
+import {ContactComponent} from './contact/contact.component';
+import {PrivateProductComponent} from './private-products/private-product/private-product.component';
+import {PrivateProductsComponent} from './private-products/private-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChurchesComponent,
+    ChurchComponent,
+    ContactComponent,
+    DashboardComponent,
+    ProductionComponent,
+    FooterComponent,
+    PublicProductsComponent,
+    PublicChurchProductsComponent,
+    PublicProductComponent,
+    PublicChurchProductComponent,
+    PrivateProductComponent,
+    PrivateProductsComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +39,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
